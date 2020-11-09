dd978ac26b1d55f29746bf2dedccbb414f4002af
Anikin Pavel <anikin_p@email>
2020-11-09 19:08:00 +0400

    change 1

--- src/main/java/com/pasha/ExampleRest/ExampleRestApplication.java
+++ src/main/java/com/pasha/ExampleRest/ExampleRestApplication.java
@@ -1,13 +1,35 @@
 package com.pasha.ExampleRest;
 
+import com.pasha.ExampleRest.entities.Address;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ExampleRestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExampleRestApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(ExampleRestApplication.class);
 
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleRestApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Address address = restTemplate.getForObject(" http://{some.url}/addr/getaddrdata/{addr_id}",
+                    Address.class);
+            log.info(address.toString());
+        };
+    }
 }
